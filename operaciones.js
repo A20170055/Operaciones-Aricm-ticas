ab3c8d4fc7252b1cff6467746ac4300990d8fef2
A20170055 <a20170055@juan23.edu.pe>
2024-11-04 13:19:01 -0500

    Add files via upload

---
+++ operaciones.js
@@ -0,0 +1,24 @@
+document.write("<h1>Operaciones Básicas</h1>");
+
+var num1 = parseInt(prompt("Ingresa Primer Número:"));
+var num2 = parseInt(prompt("Ingresa Segundo Número:"));
+
+var sum = num1 + num2;
+var res = num1 - num2;
+var mul = num1 * num2;
+var div = num1 / num2;
+
+document.write("El Primer Número Fue: " + num1);
+document.write("<br>");
+document.write("El Segundo Número Fue: " + num2);
+document.write("<br>");
+document.write("La Suma De Ambos Es: " + sum);
+document.write("<br>");
+document.write("La Resta De Ambos Es: " + res);
+document.write("<br>");
+document.write("El Producto De Ambos Es: " + mul);
+document.write("<br>");
+document.write("La Divición De Ambos Es: " + div);
+document.write("<br>");
+document.write("<br>");
+document.write("<marquee><h2>Hecho Por Miguel García</h2></marquee>");
